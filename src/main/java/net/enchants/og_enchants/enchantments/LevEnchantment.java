fd18adcc5075fc71d9b0169e4bb661fb4a605815
BuildTools <unconfigured@null.spigotmc.org>
2022-08-24 21:21:47 -0400

    Initial commit

---
+++ src/main/java/net/enchants/og_enchants/enchantments/LevEnchantment.java
@@ -0,0 +1,25 @@
+package net.enchants.og_enchants.enchantments;
+
+import net.minecraft.enchantment.Enchantment;
+import net.minecraft.enchantment.EnchantmentTarget;
+import net.minecraft.entity.*;
+import net.minecraft.entity.effect.StatusEffectInstance;
+import net.minecraft.entity.effect.StatusEffects;
+
+
+
+public class LevEnchantment extends Enchantment {
+    public LevEnchantment(Rarity weight, EnchantmentTarget type, EquipmentSlot... slotTypes) {
+        super(weight, type, slotTypes);
+    }
+
+    @Override
+    public void onTargetDamaged(LivingEntity user, Entity target, int level) {
+        if(target instanceof LivingEntity) {
+            ((LivingEntity) target).addStatusEffect(new StatusEffectInstance(StatusEffects.LEVITATION, 200, 1), user);
+        }
+    }
+
+
+}
+
